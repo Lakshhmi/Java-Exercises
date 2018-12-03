38ba2c493f270e07dfe2f751c4c40266cc1e348f
Lakshmi <Lakshhmi@gmail.com>
2018-12-03 13:42:33 -0500

    Call methods from main

---
+++ Methods.java
@@ -0,0 +1,42 @@
+package ExNov24;
+
+public class Methods {
+    public int a = 0;
+    public int b = 0;
+    public int c = 0;
+
+    public static void main(String[] args) {
+
+        System.out.println("Methods :");
+        Methods methodObj = new Methods();
+        methodObj.method1();
+        methodObj.method2();
+        methodObj.method3();
+        System.out.println("Sum of Methods = " + (methodObj.a + methodObj.b + methodObj.c));
+
+    }
+
+        public  void method1() {
+            int x = 10;
+            int y = 3;
+            a = x + y;
+            System.out.println("Method 1 = " + (x + y));
+        }
+        public  void method2() {
+            int x = 5;
+            int y = 6;
+            b = x + y;
+            System.out.println("Method 2 = " + (x + y));
+        }
+        public  void method3() {
+            int x = 20;
+            int y = 14;
+            c = x + y;
+            System.out.println("Method 3 = " + (x + y));
+
+        }
+
+}
+
+
+
