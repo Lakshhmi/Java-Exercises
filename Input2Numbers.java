56b9ec5e509614ac91be5c6329348de08d3567a9
Lakshmi <Lakshhmi@gmail.com>
2018-12-03 13:41:22 -0500

    Input numbers

--- Input2Numbers.java
+++ Input2Numbers.java
@@ -1,7 +1,14 @@
 package ExNov24;
-
+import java.util.Scanner;
 public class Input2Numbers {
-    public static void main(System[] argS){
-        
+    public static void main(String[] argS){
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input first number");
+        int x = input.nextInt();
+        System.out.println("Input second number");
+        int y = input.nextInt();
+        int z =x*y;
+        System.out.println("Product of " +x+ " and " +y +" = "+ z);
     }
 }
