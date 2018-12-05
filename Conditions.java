25f4f824dfe0d04923525220b7b221a3f3989b7b
Lakshmi <Lakshhmi@gmail.com>
2018-12-05 14:42:18 -0500

    Conditions

---
+++ Conditions.java
@@ -0,0 +1,37 @@
+package ExDec01;
+import java.util.Scanner;
+
+public class Conditions {
+    char grade;
+
+    public static void main(String[] argS){
+
+        Conditions obj = new Conditions();
+        obj.Getgrade();
+        System.out.println(obj.grade);
+    }
+
+
+    public char Getgrade(){
+
+        int testScore;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter Test Score : ");
+        testScore = input.nextInt();
+
+        if(testScore >= 90 && testScore <= 100) {
+            grade = 'A';
+
+        }else if(testScore >= 80 && testScore < 90){
+             grade = 'B';
+
+        }else if(testScore >= 70 && testScore < 80) {
+            grade = 'C';
+
+        }else {
+            System.out.println("Invalid Score");
+        }
+
+        return(grade);
+    }
+}
