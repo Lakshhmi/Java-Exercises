00b8cd4fe925db0b19918ce51ade89431992118b
Lakshmi <Lakshhmi@gmail.com>
2018-12-11 18:49:22 -0500

    Methods in java

--- CallMethods.java
+++ CallMethods.java
@@ -1,14 +1,20 @@
 package ExDec09;
 
 public class CallMethods {
+
+    // Static Methods
+
+    // void - LearnPrint is not return anything
     static void LearnPrint(){
         System.out.println("I'm executed from LearnPrint");
     }
 
+    // LearnParameter method takes parameter and not return anything
     static void LearnParameter(String name){
         System.out.println(name);
     }
 
+    // int - LearnAddition returns integer
     static int LearnAddition(int x, int y){
         int z = x+y;
         return z;
