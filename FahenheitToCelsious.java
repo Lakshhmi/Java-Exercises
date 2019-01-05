cef6aa4b9bde04ccb617facd9189d17d0492848e
Lakshmi <Lakshhmi@gmail.com>
2019-01-04 22:35:28 -0500

    F to C

---
+++ FahenheitToCelsious.java
@@ -0,0 +1,14 @@
+package ExDec01;
+
+import java.util.Scanner;
+public class FahenheitToCelsious {
+    public static void main(String[] args){
+
+        System.out.println("Enter temperature in Fahrenheit :");
+
+        Scanner input =new Scanner(System.in);
+      
+
+        System.out.println("Temperature in Celsious : " + Celsious);
+    }
+}
