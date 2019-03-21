44849562605c233b6b2b5314efdeba605ef7e3a6
Lakshmi <35077924+Lakshhmi@users.noreply.github.com>
2019-03-21 11:11:24 -0400

    Java Program

---
+++ Person.java
@@ -0,0 +1,13 @@
+package JavaOOPsConcepts;
+
+public class Person {
+    String fName = "John";
+    String lName = "Elton";
+    int Age = 42;
+
+//    Static method accessed without creating an object of the class but it should be in same class
+    static void work(){
+        System.out.println("John Elton is working in Atlanta");
+    }
+
+}
