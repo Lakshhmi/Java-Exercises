d9e99056425dc3df35b8886b267eaae1d8fb8f24
Lakshmi <35077924+Lakshhmi@users.noreply.github.com>
2019-04-01 17:35:48 -0400

    Car.java

---
+++ Car.java
@@ -0,0 +1,12 @@
+package JavaOOPsConcepts;
+
+public class Car {
+    int modelYear;
+    String modelName;
+
+    //Constructor class with parameters
+    public Car(int Year,String Name){
+        modelYear = Year;
+        modelName = Name;
+    }
+}
