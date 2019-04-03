0c15a7fa858bba75b132578aecee1fb5bda36a74
Lakshmi <35077924+Lakshhmi@users.noreply.github.com>
2019-04-03 10:35:26 -0400

    MyClass.java

---
+++ MyClass.java
@@ -0,0 +1,25 @@
+package JavaOOPsConcepts;
+
+//import static JavaOOPsConcepts.Person.work;
+
+public class MyClass {
+
+
+    public static void main(String[] args){
+        System.out.println("From main class");
+        //Arithmetic Calculation
+        ArithmeticCalculation objAC = new ArithmeticCalculation();
+        System.out.println("Addition " + (objAC.i + objAC.j));
+
+
+        //Call Person class
+        Person objP = new Person();
+        System.out.println((objP.fName + " " + objP.lName) + " is " + objP.Age + " years old.");
+        objP.work();
+
+        //Call Car Class - Constructor class with parameters
+        Car myCar = new Car(2010, "Honda");
+
+        System.out.println("My car is " + myCar.modelName + " " + myCar.modelYear + " model");
+    }
+}
