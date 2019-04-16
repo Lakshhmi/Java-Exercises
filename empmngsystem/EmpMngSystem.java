e64002cae886083cbbbe9839dd726e86f209ce00
Lakshmi <35077924+Lakshhmi@users.noreply.github.com>
2019-04-16 15:43:56 -0400

    Employee Management System

--- empmngsystem/EmpMngSystem.java
+++ empmngsystem/EmpMngSystem.java
@@ -4,8 +4,8 @@ public class EmpMngSystem {
     public static void main(String argS[]){
 
         EmployeeManager empMng = new EmployeeManager();
-        empMng.addupdateEmployee("Laks","S","Feb18","Active");
-        empMng.addupdateEmployee("Mani","R","Apr8","Active");
+        empMng.addupdateEmployee("John","S","Feb18","Active");
+        empMng.addupdateEmployee("Nick","R","Apr8","Active");
 
     }
 }
