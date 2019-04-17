b06fbfc56a32e920c43fd46fdbe35af347930398
Lakshmi <35077924+Lakshhmi@users.noreply.github.com>
2019-04-17 16:46:39 -0400

    Employee Management System

--- empmngsystem/Notifier.java
+++ empmngsystem/Notifier.java
@@ -3,5 +3,5 @@ package com.basic2.empmngsystem;
 interface Notifier {
     public void AddObserver();
     public void RemoveObserver();
-    public void Notifier();
+    public void Notifier(Employee Emp,String Status);
 }
