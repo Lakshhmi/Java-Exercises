932864ac71901567db8ae2cc55b694497ca68862
Lakshmi <35077924+Lakshhmi@users.noreply.github.com>
2019-05-06 14:57:39 -0400

    Database Connection

---
+++ PracticeDbConnection.java
@@ -0,0 +1,20 @@
+package com.basic2.db_connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PracticeDbConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/db_empmanagement";
+        String userName = "root";
+        String password = "Career4us";
+        String sql = "";
+        try {
+            Connection conn = DriverManager.getConnection(url, userName, password);
+
+        } catch (Exception e) {
+            System.out.println(e);
+            e.printStackTrace();
+        }
+    }
+}
